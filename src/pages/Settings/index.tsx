e52cb4adbb46faa54a3775e601bf456d89d3021e
dohaicuong <beatyshot@gmail.com>
2020-06-04 15:55:44 +1000

    i18n language settings

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { useAppContext } from 'App'
+import { useTranslation } from 'react-i18next'
 
 const Settings = () => {
   const { changeTheme } = useAppContext()
+  const { i18n } = useTranslation()
 
   return (
     <>
-      <button onClick={() => changeTheme('Sophia')}>Sophia</button>
-      <button onClick={() => changeTheme('Jade')}>Jade</button>
+      <div>
+        <button onClick={() => changeTheme('Sophia')}>Sophia</button>
+        <button onClick={() => changeTheme('Jade')}>Jade</button>
+      </div>
+      <div>
+        <button onClick={() => i18n.changeLanguage('en')}>English</button>
+        <button onClick={() => i18n.changeLanguage('vi')}>Vietnamese</button>
+      </div>
     </>
   )
 }
