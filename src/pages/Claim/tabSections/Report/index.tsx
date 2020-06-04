8c29b3534734cab48b3adc5bb73549927851b534
dohaicuong <beatyshot@gmail.com>
2020-06-05 09:12:21 +1000

    report next step action

--- src/pages/Claim/tabSections/Report/index.tsx
+++ src/pages/Claim/tabSections/Report/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import { useClaimAction } from 'pages/Claim/actions'
+import { Paper, Button, makeStyles } from '@material-ui/core'
 
 const Report = () => {
+  const classes = useStyles()
   const { nextStep } = useClaimAction()
 
   return (
-    <>
-      Report
-      <button onClick={nextStep.handleOpen}>next step</button>
-    </>
+    <div className={classes.root}>
+      <Paper className={classes.actionRoot}>
+        <Button
+          color="primary"
+          variant="outlined"
+          size="large"
+          onClick={nextStep.handleOpen}
+        >
+          Next Step
+        </Button>
+      </Paper>
+    </div>
   )
 }
 export default Report
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(),
+  },
+  actionRoot: {
+    padding: theme.spacing(),
+  },
+}))
