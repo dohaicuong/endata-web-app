2b72037a2d71b336d6626e65dbb8c767bc14eb59
dohaicuong <beatyshot@gmail.com>
2020-06-08 12:37:17 +1000

    404 redirect

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-// import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 const NotFound = () => {
-  // const token = localStorage.getItem('ACCESS_TOKEN')
+  const token = localStorage.getItem('ACCESS_TOKEN')
 
-  // const { push } = useHistory()
-  // React.useEffect(() => {
-  //   if (!token) push('/user/login')
-  //   else push('/claims')
-  // }, [token, push])
+  const { push } = useHistory()
+  React.useEffect(() => {
+    if (!token) push('/user/login')
+    else push('/claims')
+  }, [token, push])
 
   return <>Not found 404</>
 }
